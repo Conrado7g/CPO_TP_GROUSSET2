ef545cae3cbde35d09c341528aa7a227aa3d0bba
Conrado Grousset <conradogrousset@gmail.com>
2026-01-02 12:07:19 +0100

    Test GrilleDeJeu OK

--- LightOff_GROUSSET_version_console/src/lightoff_grousset_version_console/LightOff_GROUSSET_version_console.java
+++ LightOff_GROUSSET_version_console/src/lightoff_grousset_version_console/LightOff_GROUSSET_version_console.java
@@ -2,22 +2,31 @@ package lightoff_grousset_version_console;
 
 public class LightOff_GROUSSET_version_console {
 
-    public static void main (String[] args) {
+   public static void main(String[] args) {
 
-        CelluleLumineuse c1 = new CelluleLumineuse();
-        CelluleLumineuse c2 = new CelluleLumineuse();
+    GrilleDeJeu g = new GrilleDeJeu(7, 7);
 
-        System.out.println("Etat initial c1 : " + c1); // O
-        System.out.println("Etat initial c2 : " + c2); // O
+    System.out.println("Grille initiale :");
+    System.out.println(g);
 
-        c1.activerCellule();
-        System.out.println("Après activerCellule(), c1 : " + c1); // X
+    g.activerLigneDeCellules(0);
+    System.out.println("Après activation ligne 0 :");
+    System.out.println(g);
 
-        c1.eteindreCellule();
-        System.out.println("Après eteindreCellule(), c1 : " + c1); // O
+    g.activerColonneDeCellules(0);
+    System.out.println("Après activation colonne 0 :");
+    System.out.println(g);
 
-        System.out.println("c1 est eteint ? " + c1.estEteint()); // true
-        System.out.println("Etat brut c1 (getEtat) : " + c1.getEtat()); // false
-    }
+    g.activerDiagonaleDescendante();
+    System.out.println("Après activation diagonale descendante :");
+    System.out.println(g);
+
+    g.melangerMatriceAleatoirement(10);
+    System.out.println("Après mélange :");
+    System.out.println(g);
+
+    System.out.println("Tout éteint ? " + g.cellulesToutesEteintes());
+}
+
 }
 
